3243241e4ccedd88df7055d16cd6612d5bb746ee
Loke <nguyenvanminhcuong2002@gmail.com>
2024-03-25 14:06:05 +0700

    commit

--- command.sh
+++ command.sh
@@ -1,1 +1,2 @@
 docker build . -t vgg11_cat_and_dog_restapi
+docker run --rm -p 8080:5003 --name cat_and_dog_ai  vgg11_cat_and_dog_restapi
--- test_api.py
+++ test_api.py
@@ -2,7 +2,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "http://127.0.0.1:5000/vgg11_cat_and_dog"
+    url = "http://127.0.0.1:8080/vgg11_cat_and_dog"
 
     response = requests.post(url, files={"image": open('./img/dog_01.jpg', 'rb')})
 
